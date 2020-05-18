db2e6cc7aa2a76ee065c97c423d8034e97de7aba
Molnar Emese <mse212002@gmail.com>
2020-05-18 11:50:31 +0300

    :sparkles: implemented basic file structure, App and Book components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Bookstore</h1>
+      <BookList />
+      <BooksForm />
     </div>
   );
 }
---
+++ src/components/Book.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Book extends Component {
+  state = {};
+  render() {
+    const book = this.props.book;
+
+    return (
+      <tr></tr>
+    );
+  }
+}
+
+export default Book;
