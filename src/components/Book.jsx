843146ec09ac3959a0288626f9365ba5df5a99c1
Mesi21 <mse212002@gmail.com>
2020-05-21 18:09:09 +0300

    :art: changed the file extension to jsx for App and Book component

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Book extends React.Component {
-  render() {
-    const { id, title, category } = this.props;
-    return (
-      <tr>
-        <td>{id}</td>
-        <td>{title}</td>
-        <td>{category}</td>
-      </tr>
-    );
-  }
-}
+const Book = ({ id, title, category }) => (
+  <tr>
+    <td>{id}</td>
+    <td>{title}</td>
+    <td>{category}</td>
+  </tr>
+);
 
 Book.propTypes = {
   id: PropTypes.number.isRequired,
