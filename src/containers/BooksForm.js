61340442a57437a50a1589e00dbd19477cff1419
Mesi21 <mse212002@gmail.com>
2020-05-21 19:52:46 +0300

    :art: Transformed BooksForm to class and added constructor / state

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 const category = [
   { id: 1, name: 'Action' },
@@ -10,31 +10,41 @@ const category = [
   { id: 7, name: 'Sci-fi' },
 ];
 
-const BooksForm = () => (
-  <div>
-    <form>
-      <label htmlFor="title">
-        The Title of the Book
-        <input
-          type="text"
-          placeholder="enter the title of the book"
-          name="title"
-          id="title"
-        />
-      </label>
-      <label htmlFor="select">
-        <select>
-          {category.map(c => (
-            <option key={c.id}>{c.name}</option>
-          ))}
-        </select>
-      </label>
-      <button type="submit" value="submit">
-        Submit
-      </button>
-    </form>
-  </div>
-);
+class BooksForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
 
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <form>
+          <label htmlFor="title">
+            The Title of the Book
+            <input
+              type="text"
+              placeholder="enter the title of the book"
+              name="title"
+              id="title"
+            />
+          </label>
+          <label htmlFor="select">
+            <select>
+              {category.map(c => (
+                <option key={c.id}>{c.name}</option>
+              ))}
+            </select>
+          </label>
+          <button type="submit" value="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default BooksForm;
